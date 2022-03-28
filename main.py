af1b7f03be57b622453327d724f8368a6190ce01
Фёдор Кононов <92929240+billkol@users.noreply.github.com>
2022-03-28 18:07:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -99,7 +99,8 @@ def handle_dialog(req, res):
     ]:
         # Пользователь согласился, прощаемся.
         res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!\nА теперь купи кролика'
-        # by_rebbit(request.json, response)
+        res['response']['end_session'] = True
+        by_rebbit(request.json, response)
         return
 
     # Если нет, то убеждаем его купить слона!
